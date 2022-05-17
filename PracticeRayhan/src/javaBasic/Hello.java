db0d416d98259b7f1469ebbbecb326cb0ebc85af
ryhnchwdhry <ryhnchwdhry@gmail.com>
2022-05-17 01:27:49 -0400

    aster

---
+++ PracticeRayhan/src/javaBasic/Hello.java
@@ -0,0 +1,12 @@
+package javaBasic;
+
+public class Hello {
+public static void main(String[] args) {
+	System.out.println("rayhan");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
